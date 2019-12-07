e6ff43439ce50826a01acb75d21c4aca43242525
Blagushin Rostislav <blagushin.r.f@sberbank.ru>
2019-12-07 18:09:52 +0300

    Remove scheduler

--- src/main/java/ros/hack/bonuses/RosHackApplication.java
+++ src/main/java/ros/hack/bonuses/RosHackApplication.java
@@ -3,10 +3,8 @@ package ros.hack.bonuses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableKafka
 public class RosHackApplication {
     public static void main(String[] args) {
