e8ba602801350c8df7f6863e6882c0f0bfba8e38
Blagushin Rostislav <blagushin.r.f@sberbank.ru>
2019-12-08 07:29:06 +0300

    Final fixes

--- src/main/java/ros/hack/bonuses/consts/Constants.java
+++ src/main/java/ros/hack/bonuses/consts/Constants.java
@@ -2,7 +2,7 @@ package ros.hack.bonuses.consts;
 
 public class Constants {
 
-    public static final String SERVICE_NAME = "bonus";
+    public static final String SERVICE_NAME = "BONUSES";
     public static final String AMOUNT = "amount";
     public static final String CASHBACK = "cashback";
 }
